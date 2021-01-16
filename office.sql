983d7942a7181568d1d9f81bcf6b93a10896191c
Marin Andreev <48124400+marinandreew@users.noreply.github.com>
2021-01-16 20:36:22 +0200

    updated database

--- office.sql
+++ office.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2021 at 06:34 PM
+-- Generation Time: Jan 16, 2021 at 07:35 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
@@ -32,6 +32,7 @@ CREATE TABLE `users` (
   `firstName` varchar(255) CHARACTER SET utf8 NOT NULL,
   `middleName` varchar(255) CHARACTER SET utf8 NOT NULL,
   `lastName` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `position` varchar(250) CHARACTER SET utf8 NOT NULL,
   `phoneNumber` int(10) NOT NULL,
   `username` varchar(11) CHARACTER SET utf8 NOT NULL,
   `email` varchar(255) CHARACTER SET utf8 NOT NULL,
@@ -48,18 +49,3 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
