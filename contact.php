5792680f75b8259597043dbfef33ff104657412b
Marin Andreev <48124400+marinandreew@users.noreply.github.com>
2021-01-17 10:05:38 +0200

    updated contacts

--- contact.php
+++ contact.php
@@ -6,11 +6,13 @@
         <title>Our Office News</title>
   </head>
   <body>
+    <div class="container1">
     <form method="POST" action="send.php">		
-		<input type="text" name="name" class="inputs" placeholder="title" required>
-		<input type="text" name="email" class="inputs" placeholder="username" required>
-		<textarea name="message" class="inputs" placeholder="Message" required></textarea>
+		<input type="text" name="name" class="inputs" placeholder="Заглавие" required>
+		<input type="email" name="email" class="inputs" placeholder="Username на получател" required>
+		<textarea name="message" class="inputs" placeholder="Съобщение" required></textarea>
 		<button type="submit">Send</button>
 	</form>
+  </div>
 </body>
 </html>
