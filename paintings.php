7eaaacfe67b70917fdf88acc25a57465a6906523
Ludfik <venito1234@abv.bg>
2021-01-17 04:06:22 +0200

    paintings & pictures changes
    better view

--- paintings.php
+++ paintings.php
@@ -1,21 +1,43 @@
-
+<!DOCTYPE html>
 <html>
-    <body>
-        <form action="add.php" method="post" enctype="multipart/form-data">
-            <h4 class="parametr">Параметри</h4>
-            <p>Заглавие:</p>
-            <input id="title" name="title" maxlength="50" class="input-title" required>
-            <p>Категория:</p>
+  <head>
+        <title>OON - Home</title>
+        <link rel="stylesheet" href="main.css"/>
+  </head>
+  <body>
+    <div class="navbar">
+        <div class="container">
+        <div class="logo_div">
+            <a href="index.html"><img src="images/logo.png"  class="logo"></a>
+        </div>
+        <div class="navbar_links">
+            <ul class="menu">
+                <li><a href="index.html">Home</a></li>
+                <li class="nav-item">
+                    <a href="myprofile.html" class="nav-link">My profile</a>
+                </li>
+                <li><a href="about.html">About us</a></li>
+                <li><a href="login.html">Log in</a></li>
+                <li><a href="registration.html">Registration</a></li>
+                </ul>                            
+            </div>
+        </div>
+    </div>
+    <form action="add.php" method="post" enctype="multipart/form-data">
+            <h4 class="parametr" name="parametr">Параметри</h4>
+            <p name="titles">Заглавие:</p>
+            <input id="title" name="title" maxlength="50" class="inputs" required>
+            <p name="titles">Категория:</p>
             <p>
             <select id="news_type" name="news_type" class="select">
-                <option value="1" data-marker="1">новина</option>
-                <option value="2" data-marker="2">задание за работа</option>
+                <option value="1" data-marker="1">Новина</option>
+                <option value="2" data-marker="2">Свободна Позиция</option>
             </select>
             </p>
-            <p>Описание:</p>
-            <textarea id="description" name="description" maxlength="600" class="input-description" placeholder="" style="height: 130px;" required></textarea>
-            <p>.</p>
-            <input type="submit" name="upload" value="Континиу">
-        </form>
+            <p name="titles">Описание:</p>
+            <input id="description" name="description" maxlength="50" class="inputs" placeholder="" style="height: 130px;" required></input>
+            <br>
+            <input type="submit" name="upload" value="Потвърди">
+        </form> 
     </body>
-</html>
+    </html>
--- pictures.php
+++ pictures.php
@@ -1,17 +1,33 @@
-
-    
 <!DOCTYPE html>
 <html>
   <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/our.css"/>
+        <link rel="stylesheet" href="style.css"/>
+        <title>OON - Home</title>
   </head>
   <body>
-
+  <div class="navbar">
+        <div class="container">
+        <div class="logo_div">
+            <a href="index.html"><img src="images/logo.png"  class="logo"></a>
+        </div>
+        <div class="navbar_links">
+            <ul class="menu">
+                <li><a href="index.html">Home</a></li>
+                <li class="nav-item">
+                    <a href="myprofile.html" class="nav-link">My profile</a>
+                </li>
+                <li><a href="about.html">About us</a></li>
+                <li><a href="login.html">Log in</a></li>
+                <li><a href="registration.html">Registration</a></li>
+                </ul>                            
+            </div>
+        </div>
+    </div>
 <?php
-    $server = $_SERVER['SERVER_ADDR'];
+    $server = 'localhost' ;
     $username = 'root';
-    $password = 'root';
+    $password = '';
     $dbname = 'office';
     $charset = 'utf8';
 
@@ -31,19 +47,28 @@
 	while($row = $query->fetch_assoc()){
 ?>
 
-            <div class="second">
-                <div class="cell" data-title="Име"><?php echo($row['title']);?></div>
+ 
+         
+              <div class="second">
+              <form>   
+              <div class="cell" data-title="Име"><?php echo($row['title']);?></div>
                 <div class="cell-di" data-title="Описание"><?php echo($row['description']);?></div>
-                <div class="cell" data-title="Категория"><?php if($row['news_type'] == 1){
-                        echo "новина";
+                <div class="cell" data-title="Категория" name="category"><?php if($row['news_type'] == 1){
+                        echo "Новина";
                         }
                         else{
-                        echo "задание за работа";
+                        echo "Задание за работа";
                         ?> <div class="cell" data-title="Изтрий"><a  href="?&delete=&id=<?php echo($row['id']);?>"><input class="button" type="submit" value="Приеми"></a></div>
                         <?php
-                            }?></div>
-                
-            </div>
+                            }?>
+                               
+                          </div>
+                          </form>
+                          <br>
+                          <br>       
+                        </div>
+                          
 
-    <?php }}else{header("Location: paintings.php");} ?></div></body></html>
+    <?php }}else{header("Location: paintings.php");} ?></div>
+  </body></html>
     
